70e5a08584112620966c3eb983416322727332bd
Allan Buhl Blindbæk <abbl52712@edu.ucl.dk>
2023-04-20 14:08:41 +0200

    Med property i Employee

--- GettingReal/Employee.cs
+++ GettingReal/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,6 @@ namespace GettingReal
 {
     public class Employee
     {
+        public string role { get; set; }
     }
 }
