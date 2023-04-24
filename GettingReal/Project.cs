e189a3c774b8f9e9f0952dddeda31ecedd8aa11c
Allan Buhl Blindbæk <abbl52712@edu.ucl.dk>
2023-04-24 08:21:56 +0200

    Med properties og constructor på Resource og Project

--- GettingReal/Project.cs
+++ GettingReal/Project.cs
@@ -8,5 +8,13 @@ namespace GettingReal
 {
     public class Project
     {
+        public string Name { get; set; }
+        public bool Finished { get; set; }
+
+        public Project(string name, bool finished)
+        {
+            Name = name;
+            Finished = finished;
+        }
     }
 }
--- GettingReal/Resource.cs
+++ GettingReal/Resource.cs
@@ -8,5 +8,17 @@ namespace GettingReal
 {
     public class Resource
     {
+        public string Type { get; set; }
+        public int Amount { get; set; }
+        public bool Rented { get; set; }
+        public int Id { get; set; }
+
+        public Resource (string type, int amount, bool rented, int id)
+        {
+            Type = type;
+            Amount = amount;
+            Rented = rented;
+            Id = id;
+        }
     }
 }
