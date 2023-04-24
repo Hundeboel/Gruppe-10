23c1cf3f3bf35345745552e4288f286eca76ec90
Sebastian Hundebøl Hansen <Shha54115@edu.ucl.dk>
2023-04-24 08:39:43 +0200

    Fixed fasteners

--- GettingReal/Fastener.cs
+++ GettingReal/Fastener.cs
@@ -9,13 +9,15 @@ namespace GettingReal
     public class Fastener
     {
         public string Type { get; set; }
-        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
         public int Id { get; set; }
 
-        public Fastener(string type, string name, int id) 
+        public Fastener(string type, string description,int amount, int id) 
         {
             Type = type;
-            Name = name;
+            Description = description;
+            Amount = amount;
             Id = id;
         }
     }
