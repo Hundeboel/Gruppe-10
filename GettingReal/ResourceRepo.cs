63b2460ff5914165424692a5de7b443eb288399e
Allan Buhl Blindbæk <abbl52712@edu.ucl.dk>
2023-05-01 09:57:31 +0200

    Med ResourceRepo

--- GettingReal/ResourceRepo.cs
+++ GettingReal/ResourceRepo.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace WPFapp
 {
-    internal class ResourceRepo
+    public class ResourceRepo
     {
+        private List<Resource> resources;
+
+        public int Count
+        {
+            get { return resources.Count; }
+        }
+
+        public ResourceRepo()
+        {
+            resources = new List<Resource>();
+        }
+
+        public void AddResource(Resource resource)
+        {
+            resources.Add(resource);
+        }
+
+        public Resource GetResourceAtIndex(int index)
+        {
+            Resource resourceResult = null;
+            if (index >= 0 && index < resources.Count)
+            {
+                resourceResult = resources[index];
+            }
+
+            return resourceResult;
+        }
+
+        public void RemoveResourceAtIndex(int index)
+        {
+            if (index >= 0 && index < resources.Count)
+            {
+                resources.RemoveAt(index);
+            }
+        }
+
+        public void RemoveResource(Resource resource)
+        {
+            if (resource != null)
+            {
+                resources.Remove(resource);
+            }
+        }
     }
 }
