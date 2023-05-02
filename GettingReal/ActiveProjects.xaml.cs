95077778f1ed70afc4e9919bbf7bbd73ec4e804b
Nicklas Danielsen <npda54084@edu.ucl.dk>
2023-05-02 12:54:45 +0200

    Comitted Nicklas ProjectRepo

---
+++ GettingReal/ActiveProjects.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFapp
+{
+    /// <summary>
+    /// Interaction logic for ActiveProjects.xaml
+    /// </summary>
+    public partial class ActiveProjects : Window
+    {
+        public ActiveProjects()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MainPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- GettingReal/ProjectRepo.cs
+++ GettingReal/ProjectRepo.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace WPFapp
 {
-    internal class ProjectRepo
+    public class ProjectRepo
     {
+        private List<Project> projects;
+
+
+        public int Count
+        {
+            get { return projects.Count; }
+        }
+
+        public ProjectRepo()
+        {
+            projects = new List<Project>();
+        }
+
+
+        public void AddProject(Project project)
+        {
+            projects.Add(project);
+        }
+
+
+        /* public Project GetProjectByName(int index) 
+         { 
+             Project projectResult = null;
+             if (index >= 0 && index < projects.Count) 
+             { 
+                 projectResult = projects[index];
+             }
+
+             return projectResult;
+         }
+        */
+
+        public Project GetProjectAtIndex(int index)
+        {
+            Project projectResult = null;
+            if (index >= 0 && index < projects.Count)
+            {
+                projectResult = projects[index];
+            }
+
+            return projectResult;
+        }
+
+        public void RemoveProject(Project project)
+        {
+            if (project != null)
+            {
+                projects.Remove(project);
+            }
+        }
+
+
+
     }
 }
